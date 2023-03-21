64c597269a219de1aa4b41e15883bab447dbe5d4
Franco Cuenca <francomats424@gmail.com>
2023-03-21 13:19:06 -0300

    prueba con fetch

--- js/fetch.js
+++ js/fetch.js
@@ -1,5 +1,20 @@
+
+const promoscionesContainer = document.getElementById("promosContainer")
 fetch("../data/promos.json")
     .then(response => response.json())
     .then(data => {
-        console.log(data)
+        data.forEach(itemsPromos => {
+            const div = document.createElement("div");
+            div.classList.add("itemPromo")
+            div.innerHTML= `
+            <a href="" target="_self">
+                <div class="card">
+                    <img src="${itemsPromos.imagen}" alt="">
+                    <div>${itemsPromos.nombre}</div>
+                    <span><b>$${itemsPromos.precio}</b></span>
+                </div>
+             </a>
+            `
+            promoscionesContainer.appendChild(div)
+        });
     })
